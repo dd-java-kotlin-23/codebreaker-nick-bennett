df252f59d3d5d6fd233118f40e0b92fc06256690
nick-bennett <nbennett10@cnm.edu>
2026-06-22 15:44:42 -0600

    Start JavaFX implementation configuration.

---
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/Main.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.codebreaker.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+
+}
---
+++ javafx/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module edu.cnm.deepdive.codebreaker.javafx {
+
+  requires javafx.controls;
+  requires javafx.fxml;
+
+  exports edu.cnm.deepdive.codebreaker.javafx to javafx.graphics;
+
+  opens edu.cnm.deepdive.codebreaker.javafx to javafx.fxml;
+  opens edu.cnm.deepdive.codebreaker.javafx.controller to javafx.fxml;
+
+}
