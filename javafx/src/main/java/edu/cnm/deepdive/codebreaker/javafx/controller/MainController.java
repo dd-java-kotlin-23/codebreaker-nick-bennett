a88222fc08399a2380475e58bbb7d02b58ff6d7b
nick-bennett <nbennett10@cnm.edu>
2026-06-23 15:24:49 -0600

    Refining layout with CSS

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
@@ -10,6 +10,9 @@ import javafx.scene.control.TextField;
 public class MainController {
 
   @FXML
+  private Button settings;
+
+  @FXML
   private ListView guesses; // FIXME: 6/23/26 Add the type parameter for Guess.
 
   @FXML
@@ -29,6 +32,19 @@ public class MainController {
   }
 
   @FXML
+  void initialize() {
+    submitGuess.setDisable(true);
+    // TODO Pass a CellFactory to the guesses listview.
+    // TODO Register an observer (consumer) of Game with the viewmodel:
+    //    If game is not null:
+    //      1. Clear out the observableList of guesses that the listview is holding.
+    //      2. Add all of the guesses in the updated game to the listview's observable list.
+    // TODO Register an observer of Throwable (error) with the viewmodel:
+    //    If error is not null:
+    //      1. Display a message to the user, indicating the error. (Probably a network connection/resolution/timeout error.)
+  }
+
+  @FXML
   void startGame(ActionEvent actionEvent) {
     // TODO: 6/23/26 Use the viewmodel to start the game.
     // viewModel.startGame("ABCDEF", 3);
@@ -37,6 +53,12 @@ public class MainController {
   @FXML
   void submitGuess(ActionEvent actionEvent) {
     // TODO: 6/23/26 Submit guess for current game.
+    // viewModel.submitGuess(...)
+  }
+
+  @FXML
+  void showSettings(ActionEvent actionEvent) {
+    // TODO: 6/23/26 Open settings window.
   }
 
 }
