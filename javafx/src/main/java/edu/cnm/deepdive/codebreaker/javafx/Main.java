1c0c20ca1e72a297d7f24f51b942066d29a4a111
nick-bennett <nbennett10@cnm.edu>
2026-06-23 09:46:02 -0600

    High-level choreography in place.

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/Main.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/Main.java
@@ -1,9 +1,20 @@
 package edu.cnm.deepdive.codebreaker.javafx;
 
+import edu.cnm.deepdive.codebreaker.javafx.controller.MainController;
+import java.util.ResourceBundle;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+  private static final String BUNDLE_BASE_NAME = "bundles/ui-strings";
+  private static final String LAYOUT_NAME = "layouts/main.fxml";
+  private static final String WINDOW_TITLE_KEY = "windowTitle";
+
+  private MainController controller;
 
   static void main(String[] args) {
     launch(args);
@@ -11,7 +22,22 @@ public class Main extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
+    ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_BASE_NAME);
+    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(LAYOUT_NAME), bundle);
+    Parent root = loader.load();
+    controller = loader.getController();
+    Scene scene = new Scene(root);
+    stage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
+    stage.setResizable(false); // TODO: 6/23/26 Investigate resizing.
+    // TODO: 6/23/26 Set additional properties (e.g., icon) of stage.
+    stage.setScene(scene);
+    stage.show();
+  }
 
+  @Override
+  public void stop() throws Exception {
+    super.stop();
+    // TODO: 6/23/26 Invoke methods on controller (etc.) as necessary for shutting down.
   }
 
 }
---
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.codebreaker.javafx.controller;
+
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+  @FXML
+  private ResourceBundle resources;
+
+}
