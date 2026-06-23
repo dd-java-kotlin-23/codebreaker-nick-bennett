3cd5529514a5cbe7a550ef502ac3b8f40d5be132
nick-bennett <nbennett10@cnm.edu>
2026-06-23 14:23:20 -0600

    Building and styling layout.

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/javafx/controller/MainController.java
@@ -1,12 +1,22 @@
 package edu.cnm.deepdive.codebreaker.javafx.controller;
 
-import edu.cnm.deepdive.codebreaker.javafx.viewmodel.CodebreakerViewModel;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class MainController {
+
+  @FXML
+  private ListView guesses; // FIXME: 6/23/26 Add the type parameter for Guess.
+
+  @FXML
+  private Button submitGuess;
+
+  @FXML
+  private TextField guessInput;
 
   @FXML
   private Button startGame;
@@ -14,23 +24,19 @@ public class MainController {
   @FXML
   private ResourceBundle resources;
 
-  private CodebreakerViewModel viewModel;
-
-  public CodebreakerViewModel getViewModel() {
-    return viewModel;
-  }
-
-  public void setViewModel(CodebreakerViewModel viewModel) {
-    this.viewModel = viewModel;
-  }
-
   public void shutdown() {
-    viewModel.shutdown();
+//    viewModel.shutdown();
   }
 
   @FXML
   void startGame(ActionEvent actionEvent) {
     // TODO: 6/23/26 Use the viewmodel to start the game.
+    // viewModel.startGame("ABCDEF", 3);
+  }
+
+  @FXML
+  void submitGuess(ActionEvent actionEvent) {
+    // TODO: 6/23/26 Submit guess for current game.
   }
 
 }
