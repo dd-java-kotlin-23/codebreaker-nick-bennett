82a56292120e649e41d5f31f4ffe41f09ce19cac
nick-bennett <nbennett10@cnm.edu>
2026-06-30 11:02:17 -0600

    Building rudimentary layout.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/MainActivity.java
@@ -9,15 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.codebreaker.app.R;
+import edu.cnm.deepdive.codebreaker.app.databinding.ActivityMainBinding;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
       Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/viewmodel/GameViewModel.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import edu.cnm.deepdive.codebreaker.model.Game;
@@ -17,17 +18,23 @@ public class GameViewModel extends ViewModel {
 
   private final CodebreakerService service;
   private final MutableLiveData<Game> game;
+  private final LiveData<Boolean> solved;
   private final MutableLiveData<Throwable> error;
 
   @Inject
   public GameViewModel(CodebreakerService service) {
     this.service = service;
     game = new MutableLiveData<>();
+    solved = Transformations.map(game, Game::isSolved);
     error = new MutableLiveData<>();
   }
 
   public LiveData<Game> getGame() {
     return game;
+  }
+
+  public LiveData<Boolean> getSolved() {
+    return Transformations.distinctUntilChanged(solved);
   }
 
   public LiveData<Throwable> getError() {
@@ -38,6 +45,14 @@ public class GameViewModel extends ViewModel {
     error.setValue(null);
     service
         .startGame("ABCDEF", 3)
+        .thenAccept(game::postValue)
+        .exceptionally(this::postError);
+  }
+
+  public void submitGuess(String text) {
+    error.setValue(null);
+    service
+        .submitGuess(game.getValue(), text)
         .thenAccept(game::postValue)
         .exceptionally(this::postError);
   }
